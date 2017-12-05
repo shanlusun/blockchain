886be91a7db2ff30e44f0fb2d466e370b41f1d22
shanlu sun <shanlusun@gmail.com>
2017-12-05 14:04:55 +0800

    remove empty line with LF

--- hyperledger-fabric/setup_dev/download_docker_images.sh
+++ hyperledger-fabric/setup_dev/download_docker_images.sh
@@ -13,28 +13,22 @@ docker tag hyperledger/fabric-zookeeper:x86_64-1.0.3 hyperledger/fabric-zookeepe
 docker pull hyperledger/fabric-peer:x86_64-1.0.3
 docker rmi hyperledger/fabric-peer:latest 
 docker tag hyperledger/fabric-peer:x86_64-1.0.3 hyperledger/fabric-peer:latest
-
 docker pull hyperledger/fabric-tools:x86_64-1.0.3
 docker rmi hyperledger/fabric-tools:latest 
 docker tag hyperledger/fabric-tools:x86_64-1.0.3 hyperledger/fabric-tools:latest
-
 docker pull hyperledger/fabric-ccenv:x86_64-1.0.3
 docker rmi hyperledger/fabric-ccenv:latest 
 docker tag hyperledger/fabric-ccenv:x86_64-1.0.3 hyperledger/fabric-ccenv:x86_64-1.0.3
-
 docker pull hyperledger/fabric-couchdb:x86_64-1.0.3
 docker rmi hyperledger/fabric-couchdb:latest 
 docker tag hyperledger/fabric-couchdb:x86_64-1.0.3 hyperledger/fabric-couchdb:latest
-
 docker rmi hyperledger/fabric-baseos:x86_64-0.3.2 
 docker pull hyperledger/fabric-baseos:x86_64-0.3.2 
 docker tag hyperledger/fabric-baseos:x86_64-0.3.2 hyperledger/fabric-baseos:latest
 docker rmi hyperledger/fabric-baseimage:x86_64-0.3.2 
 docker pull hyperledger/fabric-baseimage:x86_64-0.3.2 
 docker tag hyperledger/fabric-baseimage:x86_64-0.3.2 hyperledger/fabric-baseimage:latest
-
 #https://registry-1.docker.io/v2/hyperledger/fabric-baseos
-
 #docker rmi fabric-baseimage:latest
 
 
